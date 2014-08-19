269d1a83796b53bc09c2ef173c6e375fa496b44a
Matt Giguere <matthew.giguere@gmail.com>
2014-08-19 18:05:56 -0400

    added changing CSS ex
    added example of changing CSS selectors via jQuery

---
+++ jQuery/changingCSS/script.js
@@ -0,0 +1,7 @@
+$(document).ready( function(){
+    $('div').mouseenter(function() {
+        $("div").height("200px");
+        $("div").width("200px");
+        $("div").css("border-radius", 10);
+    });
+});
