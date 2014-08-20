240f7396ea8425cf6463b087df27932dc380fa06
Matt Giguere <matthew.giguere@gmail.com>
2014-08-19 22:52:20 -0400

    added jQuery src and mouseleave

--- jQuery/changingCSS/script.js
+++ jQuery/changingCSS/script.js
@@ -1,7 +1,12 @@
-$(document).ready( function(){
+$(document).ready(function(){
     $('div').mouseenter(function() {
         $("div").height("200px");
         $("div").width("200px");
         $("div").css("border-radius", 10);
+    });
+    $('div').mouseleave(function() {
+        $('div').height('50px');
+        $("div").width("300px");
+        $('div').css('border-radius', 0);
     });
 });
