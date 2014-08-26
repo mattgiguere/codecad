4c9a782698b134f173f36640c5bfa069b034b637
Matt Giguere <matthew.giguere@gmail.com>
2014-08-26 18:09:31 -0400

    added math functions 1
    examples rounding and built in constants

---
+++ php/07Functions1/04RoundingConstants.php
@@ -0,0 +1,19 @@
+<html>
+    <p>
+    <?php
+    // Try rounding a floating point number to an integer
+    // and print it to the screen
+    $round_pi = round(M_PI);
+    echo $round_pi;
+    
+    ?>
+    </p>
+    <p>
+    <?php
+    // Try rounding a floating point number to 3 decimal places
+    // and print it to the screen
+    echo round(M_E, 3);
+    
+    ?>
+    </p>
+</html>
