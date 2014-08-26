5d186a0e55cf33389d053b95d681fceb026b96a7
Matt Giguere <matthew.giguere@gmail.com>
2014-08-26 14:56:32 -0400

    added ifelse
    added first semi-useful result from the PHP course

---
+++ php/02ConditionalsAndControlFlow/04MyIfElse.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Write your if/elseif/else statement here!
+        $item = 4;
+        if ($item == 5){
+            echo "The condition is true";
+        }
+        else {
+            echo "The condition is false";
+        }
+      ?>
+    </p>
+  </body>
+</html>
